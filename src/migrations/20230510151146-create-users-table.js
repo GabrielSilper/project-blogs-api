13c7155a2a807abc9bb69a87df5d37dab52dc348
GabrielSilper <gabrielsilper.main@gmail.com>
2023-05-10 14:07:04 -0400

    R01 - migrations necessárias criadas.

---
+++ src/migrations/20230510151146-create-users-table.js
@@ -0,0 +1,38 @@
+module.exports = {
+  /**
+   *
+   * @param {import('sequelize').QueryInterface} queryInterface
+   * @param {import('sequelize')} Sequelize
+   */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("users", {
+      id: {
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      display_name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        unique: true,
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("users");
+  },
+};
---
+++ src/migrations/20230510152802-create-blog-posts-table.js
@@ -0,0 +1,47 @@
+module.exports = {
+  /**
+   *
+   * @param {import('sequelize').QueryInterface} queryInterface
+   * @param {import('sequelize')} Sequelize
+   */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blog_posts', {
+      id: {
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('blog_posts');
+  },
+};
