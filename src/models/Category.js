f0768237197235c68bc4ed007950e6bcb7a6213c
GabrielSilper <gabrielsilper.main@gmail.com>
2023-05-10 19:46:15 -0400

    R07 - modelo Category criado, porém sem associações.

---
+++ src/models/Category.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * @param {import('sequelize').Sequelize} Sequelize
+ * @param {import('sequelize').DataTypes} DataTypes
+ */
+module.exports = (Sequelize, DataTypes) => {
+  const Category = Sequelize.define('Category', {
+    id: { primaryKey: true, type: DataTypes.INTEGER },
+    name: DataTypes.STRING,
+  });
+
+  return Category;
+};
